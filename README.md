02890717525bd63e63ae0e634a1e50d6dff1ffd3
César Alexandro Guerra González <71679922+CesarAGG@users.noreply.github.com>
2025-05-25 11:37:15 -0600

    sin info confidencial

--- README.md
+++
@@ -1,3 +0,0 @@
-# NaN-Crew-Datathon-2025-OXXO
-
-los datos originales deben ponerse en ./datos
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from datathon2025-oxxo!")
+
+
+if __name__ == "__main__":
+    main()
